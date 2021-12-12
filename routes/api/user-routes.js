ca81dbcc5a6099712389fff05327b132fffbdef8
Adam Calcasola <adam.calcasola@gmail.com>
2021-12-12 15:45:18 -0500

    initial commit

---
+++ routes/api/user-routes.js
@@ -0,0 +1,42 @@
+const router = require('express').Router();
+const {User} = require('../../models');
+
+router.get('/', (req, res) => {
+    User.findAll()
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.get('/:id', (req, res) => {
+    User.findOne({
+        where: {
+            id: req.params.id
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.post('/', (req, res) => {
+    User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.post('/:id', (req, res) => {});
+
+router.delete('/:id', (req, res) => {});
+
+module.exports = router;
