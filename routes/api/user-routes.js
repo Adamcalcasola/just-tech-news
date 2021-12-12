70b41a4cf5c79c275b2b6fe9a47fa74a94c57237
Adam Calcasola <adam.calcasola@gmail.com>
2021-12-12 18:10:21 -0500

    build server, user input, and CRUD

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -2,7 +2,9 @@ const router = require('express').Router();
 const {User} = require('../../models');
 
 router.get('/', (req, res) => {
-    User.findAll()
+    User.findAll({
+        attributes: {exclude: ['password']}
+    })
     .then(dbUserData => res.json(dbUserData))
     .catch(err => {
         console.log(err);
@@ -12,9 +14,17 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
     User.findOne({
+        attributes: {exclude: ['password']},
         where: {
             id: req.params.id
         }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({message: 'No user found with this id'});
+            return;
+        }
+        res.json(dbUserData);
     })
     .catch(err => {
         console.log(err);
@@ -35,8 +45,42 @@ router.post('/', (req, res) => {
         });
 });
 
-router.post('/:id', (req, res) => {});
+router.put('/:id', (req, res) => {
+    User.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbUserData => {
+            if (!dbUserData[0]) {
+                res.status(404).json({message: 'No user found with this id'});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
-router.delete('/:id', (req, res) => {});
+router.delete('/:id', (req, res) => {
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({message: 'No user found with this id'});
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 module.exports = router;
