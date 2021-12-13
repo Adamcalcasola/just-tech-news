af2248af40278598330c28a8b7b384eeb00a7029
Adam Calcasola <adam.calcasola@gmail.com>
2021-12-12 23:17:02 -0500

    fix

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const {Model,DataTypes} = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 // create our User model
 class User extends Model {}
@@ -34,19 +35,20 @@ User.init(
         }
     },
     {
-        // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
+        hooks: {
+            beforeCreate(userData) {
+                return bcrypt.hash(userData.password, 10).then(newUserData => {
+                    return newUserData
+                });
+            }
 
-        // pass in our imported sequelize connection (the direct connection to our database)
+        },
         sequelize,
-        // don't automatically create createdAt/updatedAt timestamp fields
         timestamps: false,
-        // don't pluralize name of database table
         freezeTableName: true,
-        // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
         underscored: true,
-        // make it so our model name stays lowercase in the database
         modelName: 'user'
-    }
+    },
 );
 
 module.exports = User;
