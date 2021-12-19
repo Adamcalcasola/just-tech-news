1058fab6c76bf7b9c9e9740880afabbb8201ff32
Adam Calcasola <adam.calcasola@gmail.com>
2021-12-19 15:36:18 -0500

    log in/out

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -59,10 +59,14 @@ router.post('/', (req, res) => {
         email: req.body.email,
         password: req.body.password
     })
-        .then(dbUserData => res.json(dbUserData))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
+        .then(dbUserData => {
+            req.session.save(() => {
+                req.session.user_id = dbUserData.id;
+                req.session.username = dbUserData.username;
+                req.session.loggedIn = true;
+
+                res.json(dbUserData);
+            });
         });
 });
 
@@ -81,7 +85,14 @@ router.post('/login', (req, res) => {
             res.status(400).json({message: 'Incorrect passwordf!'});
             return;
         }
-        res.json({user: dbUserData, message: 'You are now logged in!'});
+        
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({user: dbUserData, message: 'You are now logged in!'});
+        });
     })
 })
 
@@ -122,6 +133,16 @@ router.delete('/:id', (req, res) => {
         console.log(err);
         res.status(500).json(err);
     });
+});
+
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
 });
 
 module.exports = router;
